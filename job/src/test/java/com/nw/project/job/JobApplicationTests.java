9c4bad62aa1aa2cd9944ce89917d275358bd25fe
samniwu <799954193@qq.com>
2020-04-11 15:00:25 +0800

    增加job和consumer组件

---
+++ job/src/test/java/com/nw/project/job/JobApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nw.project.job;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
