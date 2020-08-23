8dfc6a1513022feffeed08675b5ce775c379ecf0
samniwu <799954193@qq.com>
2020-08-23 16:29:28 +0800

    提交新结构代码

--- cms-registry-service/src/main/java/com/nw/project/cmsregistryservice/config/WebSecurityConfig.java
+++ cms-registry-service/src/main/java/com/nw/project/cmsregistryservice/config/WebSecurityConfig.java
@@ -1,9 +1,10 @@
-package com.nw.project.registry.config;
+package com.nw.project.cmsregistryservice.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
---
+++ cms-user-service/src/main/java/com/niwu/project/cmsuserservice/CmsUserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.niwu.project.cmsuserservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CmsUserServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CmsUserServiceApplication.class, args);
+    }
+
+}
